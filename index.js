c2a01140482f7a530ca46b29647f96f1a1c22d16
Jason Reeder <info@jasonreeder.com>
2017-12-15 11:18:51 -0800

    Support devtools extension

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk),
+);
 
 render(
   <Provider store={store}>
