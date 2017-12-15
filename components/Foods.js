b6dc96b466375c5433741f6a3682a9a82bfc1066
Jason Reeder <info@jasonreeder.com>
2017-12-15 11:27:44 -0800

    Start basic action call

--- components/Foods.js
+++ components/Foods.js
@@ -4,7 +4,18 @@ import * as fromFoods from '../reducers/foods/api';
 
 class Foods extends Component {
   render() {
-    return <div>foods</div>;
+    const { addFood } = this.props;
+    console.log(addFood);
+    const food = {
+      id: 1,
+      name: 'spinach',
+    };
+
+    return (
+      <div className="container">
+        <div onClick={() => addFood(food)}>Add a food</div>
+      </div>
+    );
   }
 }
 
--- reducers/foods/api.js
+++ reducers/foods/api.js
@@ -2,5 +2,6 @@ import * as fromFoods from './foods';
 
 export const addFood = food =>
   async function(dispatch, getState) {
+    console.log('got here');
     dispatch(fromFoods.addFood(food));
   };
