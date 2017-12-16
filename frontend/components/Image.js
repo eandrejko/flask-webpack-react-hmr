a2266c74d6e2863d476aaf2727895241e9e2e2a3
Jason Reeder <info@jasonreeder.com>
2017-12-16 12:24:48 -0800

    Remove unused import

--- frontend/components/Image.js
+++ frontend/components/Image.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import * as fromImage from '../reducers/ui/image/api';
 import noIdea from '../../assets/images/dog/no-idea.jpg';
